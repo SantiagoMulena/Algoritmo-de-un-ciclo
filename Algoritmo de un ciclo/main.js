2c3c6f3e25f554f04a92a5bb54dd80d259137265
SantiagoMulena <107230166+SantiagoMulena@users.noreply.github.com>
2022-07-28 22:28:16 -0300

    Add files via upload

---
+++ Algoritmo de un ciclo/main.js
@@ -0,0 +1,22 @@
+//algoritmo de un ciclo
+for(let i=1; i<=3; i++){
+    let dni=prompt("Ingresa tu DNI")
+    
+        if(dni==112233){
+            console.log("Bienvenido");
+    }
+    else{
+        let=intentos=3-i;
+        if(intentos<2){
+            console.log("Dni incorrecto ,"+ intentos +" intentos restantes");
+        }
+        else{
+         console.log("Dni incorrecto ,"+ intentos +" intentos restantes");
+         
+        }
+    }       
+    
+}
+
+    
+
